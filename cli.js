319ca469cdaa4f7da793259efb4de9f8b24f9170
Alex Muturi <alex.migwi@gmail.com>
2026-08-25 23:52:23 +0300

    chore: update version and version retrival

--- cli.js
+++ cli.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import registerAuthCommands from './commands/auth.js';
 import registerWebhookCommands from './commands/webhook.js';
 import registerConfigCommands from './commands/config.js';
@@ -9,11 +12,20 @@ import registerOpenApiCommands from './commands/openapi.js';
 import registerApiCommands from './commands/api.js';
 import { checkPackageUpdate } from './lib/helpers.js';
 
-const CLI_VERSION = '0.0.7';
+
+// Resolve __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Read version from package.json
+const pkgPath = join(__dirname, 'package.json');
+const pkg = JSON.parse(await readFile(pkgPath, 'utf8'));
+const CLI_VERSION = pkg.version;
+
 const program = new Command();
 
 program
-  .name('paystack')
+  .name('paystack-cli')
   .description('Paystack Developer CLI - Build, test, and manage Paystack integrations from the command line')
   .version(CLI_VERSION);
 
